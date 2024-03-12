445d58260dd93a2b4c158c43a88d3ec4607e5dc5
joakimAndersson92 <joakim.andersson920202@gmail.com>
2024-03-12 06:38:52 +0100

    Remove prop and add style

--- src/utils/layout/header.tsx
+++ src/utils/layout/header.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 
-type Props = {}
 
-export default function Header({}: Props) {
+
+export default function Header() {
   return (
-    <div className='w-full'>
-        <ul className='flex justify-around'>
+    <div className='w-full h-24 bg-light-Background-color text-xl'>
+        <ul className='h-full flex justify-around items-center'>
             <li><Link href={"/"}>Home</Link></li>
             <li><Link href={"/projects"}>Projects</Link></li>
             <li><Link href={"/info"}>Info</Link></li>
